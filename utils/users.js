5a79202dbe9e1cc3633bc8100724ec70e21e8dcd
Amritpal Maan <sonny_maan@yahoo.com>
2020-07-24 22:37:54 +0000

    added userLeaves function

--- utils/users.js
+++ utils/users.js
@@ -14,6 +14,14 @@ function getCurrentUser(id) {
   return users.find(user => user.id === id);
 }
 
+//User Leaves chat
+function userLeaves() {
+  const index = users.findIndex(user => user.id === id);
+  if(index !== -1){
+    return users.splice(index, 1);
+  }
+}
+
 module.exports = {
   userJoin,
   getCurrentUser
