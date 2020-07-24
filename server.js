86c812a00bc4bfb0fdbbd1c7f0f7ff86a4b4065e
Amritpal Maan <sonny_maan@yahoo.com>
2020-07-24 22:33:29 +0000

    added room functionality and usernames

--- server.js
+++ server.js
@@ -13,20 +13,21 @@ const botName = 'ChatBot';
 app.use(express.static(path.join(__dirname, 'public')));
 io.on('connection', socket => {
   socket.on('joinRoom', ({username, room}) => {
-  
-  
+  const user = userJoin(socket.id, username, room);
+  socket.join(user.room);
     
 //Welcome a user to the chat
   socket.emit('message', formatMessage(botName, 'Welcome to NodeChat!'));
   
 //Broadcast a message to all other users when someone new enters the room
-  socket.broadcast.emit('message', formatMessage(botName, 'A user has entered the chat!'));
+  socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat!`));
   
   });
 
 //Listen for chat messages
   socket.on('chatMessage', (msg) => {
-    io.emit('message', formatMessage('USER', msg));
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
   });
   
 //Message when a user leaves the chat room
