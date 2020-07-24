cd64ed9034ed1e72976c163136cb14c1b5625929
Amritpal Maan <sonny_maan@yahoo.com>
2020-07-24 21:05:37 +0000

    output messages to the dom

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
 const socket = io();
 const chatForm = document.getElementById('chat-form');
 
+//Get message from server and output
 socket.on('message', message => {
   console.log(message);
+  outputMessage(message);
 });
 
 //User submits message
@@ -15,3 +17,14 @@ chatForm.addEventListener('submit', (e) => {
   //Emit message to server
   socket.emit('chatMessage', msg);
 });
+
+//Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class="meta">Sonny <span>9:12pm</span></p>
+						<p class="text">
+							#{message}
+						</p>`;
+	document.querySelector('.chat-messages').appendChild(div);
+}
