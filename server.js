5dc572c64954ea41a97f6c94e38926cf00496db0
Amritpal Maan <sonny_maan@yahoo.com>
2020-07-24 22:16:40 +0000

    added getCurrentUser and userJoin methods to implement rooms and usernames

--- server.js
+++ server.js
@@ -2,32 +2,36 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
-const formatMessage = require('./utils/messages')
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const botName = 'ChatBot';
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 io.on('connection', socket => {
+  socket.on('joinRoom', ({username, room}) => {
   
-  const botName = 'ChatBot';
   
+    
 //Welcome a user to the chat
   socket.emit('message', formatMessage(botName, 'Welcome to NodeChat!'));
   
 //Broadcast a message to all other users when someone new enters the room
   socket.broadcast.emit('message', formatMessage(botName, 'A user has entered the chat!'));
   
-//Message when a user leaves the chat room
-  socket.on('disconnect', () => {
-    io.emit('message', formatMessage(botName, 'A user has left the chat!'));
   });
-  
+
 //Listen for chat messages
   socket.on('chatMessage', (msg) => {
     io.emit('message', formatMessage('USER', msg));
+  });
+  
+//Message when a user leaves the chat room
+  socket.on('disconnect', () => {
+    io.emit('message', formatMessage(botName, 'A user has left the chat!'));
   });
   
 });
