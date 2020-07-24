e47cd91ea0e637c4b37639afc8bb4f3abf2dbb8c
Amritpal Maan <sonny_maan@yahoo.com>
2020-07-24 15:28:47 +0000

    set up socket io in the server file

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Run socket io when a client connects
+io.on('connection', socket => {
+  console.log('new web socket connection');
+});
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
